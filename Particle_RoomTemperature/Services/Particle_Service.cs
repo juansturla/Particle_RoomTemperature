bc783eab05549dbc6b404a334941fa3a4a1c4f23
Juan Sturla <jms0210@gmail.com>
2021-10-13 16:19:25 -0300

    Just local data

--- Particle_RoomTemperature/Services/Particle_Service.cs
+++ Particle_RoomTemperature/Services/Particle_Service.cs
@@ -6,18 +6,12 @@ using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
 using Particle_RoomTemperature.Models;
 
 namespace Particle_RoomTemperature.Services
 {
     public static class Particle_Service
     {
-        public static List<Measurement> GetSomeList()
-        {
-            return new List<Measurement>() { new Measurement() { Temperature = 1, Humidity = 1, DateTime = DateTime.Now } };
-        }
-
         public static ObservableCollection<Measurement> GetRoomMeasurements()
         {
             ObservableCollection<Measurement> measurements = new ObservableCollection<Measurement>();
@@ -57,54 +51,11 @@ namespace Particle_RoomTemperature.Services
 
         static string GetTemperatureMeasurements()
         {
-#if DEBUG
             return "24.0;24.0;23.0;24.0;23.0;23.0;23.0;23.0;23.0;23.0;23.0;23.0;23.0;23.0;24.0;24.0;24.0;23.0;23.0;23.0;23.0;23.0;23.0;23.0;23.0;23.0;23.0;23.0;23.0;23.0;23.0;23.0;23.0;23.0;23.0;23.0;23.0;23.0;23.0;23.0;23.0;23.0;23.0;23.0;23.0;23.0;23.0;22.0;22.0;22.0;22.0;22.0;22.0;22.0;23.0;22.0;23.0;23.0;23.0;23.0;23.0;23.0;24.0;23.0;23.0;23.0;23.0;23.0;23.0;23.0;23.0;22.0;22.0;22.0;22.0;22.0;22.0;22.0;23.0;23.0;23.0;23.0;23.0;23.0;23.0;23.0;23.0;23.0;23.0;23.0;23.0;23.0;23.0;23.0;23.0;23.0;23.0;23.0;23.0;23.0;23.0;23.0;23.0;23.0;24.0;24.0;24.0;23.0;24.0;24.0;24.0;25.0;25.0;24.0;24.0;24.0;24.0;24.0;24.0;24.0;24.0;23.0;24.0;24.0;24.0;24.0;";
-#endif
-            return Task.Run(() => GetParticleVariable("last_temps")).Result;
         }
         static string GetHumidityMeasurements()
         {
-#if DEBUG
             return "42.0;42.0;42.0;42.0;43.0;42.0;42.0;41.0;41.0;41.0;41.0;41.0;41.0;41.0;41.0;42.0;42.0;41.0;41.0;41.0;43.0;41.0;41.0;41.0;41.0;41.0;41.0;41.0;41.0;41.0;44.0;41.0;41.0;41.0;41.0;41.0;41.0;40.0;41.0;41.0;41.0;41.0;41.0;41.0;41.0;41.0;41.0;42.0;42.0;42.0;42.0;42.0;42.0;42.0;41.0;42.0;41.0;41.0;41.0;41.0;41.0;41.0;41.0;41.0;41.0;41.0;41.0;41.0;41.0;41.0;41.0;42.0;42.0;42.0;42.0;42.0;42.0;42.0;41.0;41.0;41.0;41.0;41.0;41.0;41.0;41.0;41.0;42.0;42.0;42.0;42.0;42.0;42.0;42.0;42.0;42.0;42.0;42.0;42.0;43.0;43.0;43.0;44.0;49.0;47.0;48.0;47.0;48.0;49.0;48.0;52.0;56.0;51.0;51.0;51.0;50.0;50.0;49.0;49.0;49.0;48.0;48.0;48.0;49.0;49.0;49.0;";
-#endif
-            return Task.Run(() => GetParticleVariable("last_humids")).Result;
         }
-        static async Task<string> GetParticleVariable(string particleVariable)
-        {
-            string accessToken = "2c22a76b05aad4212fb61ccef05f41b60c211911"; //This is your Particle Cloud Access Token
-            string deviceId = "32004f001051373331333230"; //This is your Particle Device Id
-
-            HttpClient client = new HttpClient
-            {
-                BaseAddress =
-                new Uri("https://api.particle.io/")
-            };
-
-            var content = new FormUrlEncodedContent(new[]
-            {
-                new KeyValuePair<string, string>("access_token", accessToken),
-            });
-
-            var result = await client.GetStringAsync("v1/devices/" + deviceId + "/" + particleVariable + "?access_token=" + accessToken);
-            var particleResponse = System.Text.Json.JsonSerializer.Deserialize<ParticleResponse>(result);
-            return particleResponse.result;
-        }
-    }
-
-    public class CoreInfo
-    {
-        public DateTime last_heard { get; set; }
-        public bool connected { get; set; }
-        public DateTime last_handshake_at { get; set; }
-        public string deviceID { get; set; }
-        public int product_id { get; set; }
-    }
-
-    public class ParticleResponse
-    {
-        public string cmd { get; set; }
-        public string name { get; set; }
-        public string result { get; set; }
-        public CoreInfo coreInfo { get; set; }
     }
 }
